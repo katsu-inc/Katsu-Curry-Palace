593034dd009f76f4dcc5d404c5856ef9b75320e1
Michelle-715 <michellesagredo1@gmail.com>
2025-07-16 11:29:17 +1000

    feat: seat offer

--- palace.py
+++ palace.py
@@ -1,3 +1,14 @@
-print("Irasshaimase! This is Katsu Curry Palace. Please take a seat!")
+def katsu_curry_palace():
+    print("Irasshaimase! This is Katsu Curry Palace. Please take a seat!")
+
+    while True:
+        response = input("Would you like to take a seat? (yes/no): ").strip().lower()
+        if response == "no":
+            print("No worries! Hope to see you again soon.")
+            return
+        elif response == "yes":
+            break
+        else:
+            print("Sorry, I didn't understand that.")
 
 print("Thank you for your order. Enjoy your katsu curry from Katsu Curry Palace! Arigatou gozaimashita!")
